efec40d574fa7129526c2421c8f606fd5fbfea5c
Saif Hussain <hsaifhussain810@gmail.com>
2025-12-20 00:19:09 +0500

    Added New Features

--- Number-Guessing-Game.py
+++ Number-Guessing-Game.py
@@ -1,6 +1,10 @@
 import random
+import time
 
-play_game = input("Do you want to play game? (YES/No) : ")
+with open("High_Score.txt", "r") as file:
+    high_score = int(file.read())
+
+play_game = input("Do you want to play game? (YES/No) : ").lower()
 
 while play_game == "yes":
     print("Choose Difficulty level")
@@ -18,6 +22,7 @@ while play_game == "yes":
         secret_number = random.randint(1,10000000000000000000000)
 
     attempts = 0
+    start_time = time.time()
   
     while True:     
     
@@ -27,8 +32,21 @@ while play_game == "yes":
         if guess == secret_number :
             print("Your Guess is right 🎉 "+""+ "The Secret Key is",secret_number)
             print ("Total Attempts ",attempts)
+            
+            end_time = time.time()
+            total_time = round((end_time - start_time), 2)
+            print("⏱️ Time Taken : ", total_time, "seconds")
+            
+            if attempts < high_score:
+                print("🏆 NEW HIGH SCORE!")
+                file = open("High_Score.txt", "w")
+                file.write(str(attempts))
+                file.close()
+                high_score = attempts
+            else:
+                print("High Score remains:", high_score)
             break
-
+        
         elif guess > secret_number :
             if guess - secret_number <=2 :
                 print("Very Close!")
@@ -46,4 +64,4 @@ while play_game == "yes":
             print("Secret number is : ",secret_number)
             break
     
-    play_game = input("Do u want to play again yes/no : ")
+    play_game = input("Do u want to play again yes/no : ").lower()
